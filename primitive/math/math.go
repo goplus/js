6583d04e8caacf04f5fe356776b5c8faadc3d5af
xushiwei <x@goplus.org>
2026-04-09 15:00:27 +0800

    primitive/math: full spec

--- primitive/math/math.go
+++ primitive/math/math.go
@@ -27,45 +27,14 @@ const (
 
 // Mathematical constants.
 const (
-	E   = 2.718281828459045 // https://oeis.org/A001113
-	Pi  = 3.141592653589793 // https://oeis.org/A000796
-	Phi = 1.618033988749895 // https://oeis.org/A001622
-
-	Sqrt2   = 1.4142135623730951 // https://oeis.org/A002193
-	SqrtE   = 1.6487212707001282 // https://oeis.org/A019774
-	SqrtPi  = 1.7724538509055159 // https://oeis.org/A002161
-	SqrtPhi = 1.272019649514069  // https://oeis.org/A139339
-
-	Ln2    = 0.6931471805599453 // https://oeis.org/A002162
-	Log2E  = 1 / Ln2
-	Ln10   = 2.302585092994046
-	Log10E = 1 / Ln10
-)
-
-// Floating-point limit values.
-const (
-	MaxFloat32             = 3.4028234663852886e+38  // 0x1p127 * (1 + (1 - 0x1p-23))
-	SmallestNonzeroFloat32 = 1.401298464324817e-45   // 1 / 2**(127 - 1 + 23)
-	MaxFloat64             = 1.7976931348623157e+308 // 0x1p1023 * (1 + (1 - 0x1p-52))
-	SmallestNonzeroFloat64 = 5e-324                  // 1 / 2**(1023 - 1 + 52)
-)
-
-// Integer limit values.
-const (
-	MaxInt    = 1<<63 - 1
-	MinInt    = -1 << 63
-	MaxInt8   = 1<<7 - 1
-	MinInt8   = -1 << 7
-	MaxInt16  = 1<<15 - 1
-	MinInt16  = -1 << 15
-	MaxInt32  = 1<<31 - 1
-	MinInt32  = -1 << 31
-	MaxInt64  = 1<<63 - 1
-	MinInt64  = -1 << 63
-	MaxUint8  = 1<<8 - 1
-	MaxUint16 = 1<<16 - 1
-	MaxUint32 = 1<<32 - 1
-	MaxUint64 = 1<<64 - 1
+	E       = 2.718281828459045 // https://oeis.org/A001113
+	LN10    = 2.302585092994046
+	LN2     = 0.6931471805599453 // https://oeis.org/A002162
+	LOG10E  = 1 / LN10
+	LOG2E   = 1 / LN2
+	PI      = 3.141592653589793  // https://oeis.org/A000796
+	SQRT1_2 = 0.7071067811865476 // https://oeis.org/A002162
+	SQRT_2  = 1.4142135623730951 // https://oeis.org/A002193
 )
 
 // ----------------------------------------------------------------------------
@@ -80,34 +49,30 @@ func Atan2(y, x primitive.Number) primitive.Number
 func Atanh(primitive.Number) primitive.Number
 func Cbrt(primitive.Number) primitive.Number
 func Ceil(primitive.Number) primitive.Number
-func Copysign(x, y primitive.Number) primitive.Number
+func Clz32(primitive.Number) primitive.Number
 func Cos(primitive.Number) primitive.Number
 func Cosh(primitive.Number) primitive.Number
-func Dim(x, y primitive.Number) primitive.Number
 func Exp(primitive.Number) primitive.Number
-func Exp2(primitive.Number) primitive.Number
 func Expm1(primitive.Number) primitive.Number
 func Floor(primitive.Number) primitive.Number
+func F16round(primitive.Number) primitive.Number
+func Fround(primitive.Number) primitive.Number
 func Hypot(p, q primitive.Number) primitive.Number
 func Imul(primitive.Number, primitive.Number) primitive.Number
-func Inf(sign int) primitive.Number
-func IsInf(f primitive.Number, sign int) bool
-func IsNaN(f primitive.Number) bool
 func Log(primitive.Number) primitive.Number
 func Log10(primitive.Number) primitive.Number
 func Log1p(primitive.Number) primitive.Number
 func Log2(primitive.Number) primitive.Number
-func Max(x, y primitive.Number) primitive.Number
-func Min(x, y primitive.Number) primitive.Number
-func Mod(x, y primitive.Number) primitive.Number
-func NaN() primitive.Number
+func Max(...primitive.Number) primitive.Number
+func Min(...primitive.Number) primitive.Number
 func Pow(x, y primitive.Number) primitive.Number
-func Pow10(n int) primitive.Number
+func Random() primitive.Number
 func Round(primitive.Number) primitive.Number
+func Sign(x primitive.Number) primitive.Number
 func Sin(primitive.Number) primitive.Number
 func Sinh(primitive.Number) primitive.Number
-func Signbit(x primitive.Number) bool
 func Sqrt(primitive.Number) primitive.Number
+func SumPrecise([]primitive.Number) primitive.Number
 func Tan(primitive.Number) primitive.Number
 func Tanh(primitive.Number) primitive.Number
 func Trunc(primitive.Number) primitive.Number
